27ff2621d486f51315dba550202c5061dfee9429
TranAnhPhuong0797 <trananhphuongbl1997@gmail.com>
2025-08-15 20:06:31 +0700

    Update: Config folder to run on CI/CD

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,6 +25,7 @@ const projects: PlaywrightTestProject[] = (['chromium', 'firefox', 'webkit'] as 
     }
   }));
 
+const isCI = !!process.env.CI;
 export default defineConfig({
   globalSetup: require.resolve('./src/support/global-setup'),
   globalTeardown: require.resolve('./src/support/global-teardown'),
@@ -74,7 +75,7 @@ export default defineConfig({
   projects: [
     BrowserFactory.createProject('chromium'),
     BrowserFactory.createProject('firefox'),
-    BrowserFactory.createProject('edge'),
+    ...(isCI ? [] : [BrowserFactory.createProject('edge')]),
 
     /* Test against mobile viewports. */
     // {
